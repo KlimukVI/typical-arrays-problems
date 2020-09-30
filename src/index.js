3b9c267ab15b607b5e6ba4482d5fd635a80eeef3
KlimukVI <KlimukVI@rambler.ru>
2020-10-01 00:18:58 +0300

    feat: add problem solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0) {return 0;}
+  if (array.length === 0) {return 0;}
+  let i = 0, minNumber = array[0];
+  while (i < array.length) {
+    if (array[i] < minNumber) {
+      minNumber = array[i];
+    }
+    i++;
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0) {return 0;}
+  if (array.length === 0) {return 0;}
+  let i = 0, maxNumber = array[0];
+  while (i < array.length) {
+    if (array[i] > maxNumber) {
+      maxNumber = array[i];
+    }
+    i++;
+  }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0) {return 0;}
+  if (array.length === 0) {return 0;}
+  let i = 0, avgNumber = 0;
+  while (i < array.length) {
+    avgNumber = avgNumber + array[i];
+    i++;
+  }
+  return avgNumber/array.length;
 }
