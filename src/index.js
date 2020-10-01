1a4496a41411acfaffd40b46affba17f56c5fafd
KlimukVI <KlimukVI@rambler.ru>
2020-10-01 14:43:19 +0300

    refactor: remake using built-in methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0) {return 0;}
+  if (array.length === 0) {return 0;}
+  return array.sort(function(a, b) { return a - b; })[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0) {return 0;}
+  if (array.length === 0) {return 0;}
+  return array.sort(function(a, b) { return b - a; })[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0) {return 0;}
+  if (array.length === 0) {return 0;}
+  return array.reduce((sum, item) => sum + item, 0)/array.length;
 }
